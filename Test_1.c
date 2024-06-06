20cde82b4966dc9b71cb9b8146c3e14884a8154a
WinnieRLi <wxli1103@gmail.com>
2024-06-06 11:31:33 +1000

    sum calculated

--- Test_1.c
+++ Test_1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main() {
+    int sum = 0;
     int count = 0;                                  // 用于计数总共有多少个满足条件的三位数
     printf("有1,2,3,4个数字，能组成互不相同的无重复数字的三位数分别是：\n");
     for (int i = 1; i <= 4; ++i) {                  // 第一个数字
@@ -9,11 +10,14 @@ int main() {
                 if (i != j && j != k && i != k) {   // 确保三个数字都不相同
                     printf("%d%d%d ", i, j, k);     // 输出当前满足条件的三位数
                     count++;                        // 增加计数
+                    sum += 100*i+10*j+k;
+                    // sum = sum + 100*i+10*j+k;
                 }
             }
         }
     }
     printf("\n总共有 %d 个满足条件的三位数。\n", count); // 输出总数
+    printf("\n满足条件的数字和为: %d.\n", sum); // print out the sum
     return 0;
 }
 //可以再计算一下满足条件的数字和为多少吗
